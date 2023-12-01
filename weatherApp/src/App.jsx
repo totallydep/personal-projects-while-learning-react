342a502305cb83534154d8e5601782e7b95e39d1
Sheharyar Khalid <sheharyar1446@outlook.com>
2023-12-01 17:59:26 +0500

    project done

--- weatherApp/src/App.jsx
+++ weatherApp/src/App.jsx
@@ -1,70 +1,66 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Windy, NoIcon, Sunny, Snowy, Cloudy, Rainy } from "./components/Icons";
 import InputField from "./components/InputField";
 import WeatherCard from "./components/WeatherCard";
 
-function App() {
+const API_KEY = "6eaa45d8e9bce90f1f3f7b3945ddbb2f";
+const API_URL = "https://api.openweathermap.org/data/2.5/weather";
+
+const getWeatherIcon = (weather) => {
+  switch (weather) {
+    case "Clear":
+      return <Sunny />;
+    case "Clouds":
+      return <Cloudy />;
+    case "Windy":
+    case "Rain":
+    case "Drizzle":
+      return <Rainy />;
+    case "Snow":
+      return <Snowy />;
+    default:
+      return <NoIcon />;
+  }
+};
+
+const App = () => {
   const [city, setCity] = useState("");
   const [data, setData] = useState({});
-  const [weatherIcon, setWeatherIcon] = useState(NoIcon);
   const [isValidCity, setIsValidCity] = useState(true);
+  const [weatherIcon, setWeatherIcon] = useState(NoIcon);
 
-  const handleSearch = (e) => {
+  const handleSearch = async (e) => {
     e.preventDefault();
-    fetchWeather();
+
+    if (city === "") {
+      setIsValidCity(false);
+      return;
+    }
+
+    try {
+      const response = await fetch(
+        `${API_URL}?q=${city}&units=metric&appid=${API_KEY}`
+      );
+
+      if (response.status === 404) {
+        setIsValidCity(false);
+        throw new Error("City not found");
+      }
+
+      const result = await response.json();
+      setData(result);
+      setIsValidCity(true);
+    } catch (error) {
+      console.error("Error fetching weather data:", error);
+      setIsValidCity(false);
+    }
+
     setCity("");
   };
 
-  const fetchWeather = () => {
-    fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=6eaa45d8e9bce90f1f3f7b3945ddbb2f`
-    )
-      .then((res) => {
-        if (res.status === 404 || city === "") {
-          setIsValidCity(false);
-          throw new Error("City not found");
-        }
-        return res.json();
-      })
-      .then((res) => {
-        setData(res);
-        setIsValidCity(true);
-      })
-      .catch((error) => {
-        console.error("Error fetching weather data:", error);
-        setIsValidCity(false);
-      });
-  };
-
   useEffect(() => {
-    switch (data.weather && data.weather[0].main) {
-      case "Clear":
-        setWeatherIcon(<Sunny />);
-        break;
-
-      case "Clouds":
-        setWeatherIcon(<Cloudy />);
-        break;
-
-      case "Windy":
-        setWeatherIcon(<Windy />);
-        break;
-
-      case "Rain":
-        setWeatherIcon(<Rainy />);
-        break;
-
-      case "Drizzle":
-        setWeatherIcon(<Rainy />);
-        break;
-
-      case "Snow":
-        setWeatherIcon(<Snowy />);
-        break;
-
-      default:
-        setWeatherIcon(<NoIcon />);
-    }
+    const weather = data.weather && data.weather[0].main;
+    setWeatherIcon(getWeatherIcon(weather));
   }, [data]);
 
   return (
@@ -78,6 +74,6 @@ function App() {
       </div>
     </div>
   );
-}
+};
 
 export default App;
